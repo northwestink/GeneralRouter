33d1a3ff2d7f3db295e5bbbb717d581997356eeb
northwest <orthwest_ink@outlook.com>
2025-03-15 16:21:13 +0800

    listen 8080 as default

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,13 @@
 #include <cstdlib>
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
-        return 1;
-    }
-
-    int port = std::atoi(argv[1]);
-    if (port <= 0 || port > 65535) {
-        std::cerr << "Invalid port number. Port must be between 1 and 65535. use 8080 as default" << std::endl;
-        port = 8080;
+    int port = 8080;
+    if (argc >= 2) {
+        port = std::atoi(argv[1]);
+        if (port <= 0 || port > 65535) {
+            std::cerr << "Invalid port number. Port must be between 1 and 65535. use 8080 as default" << std::endl;
+            port = 8080;
+        }
     }
 
     TcpServer server(port);
